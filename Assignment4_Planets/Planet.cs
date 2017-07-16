a18595db56772745e244148342c8b5b33e1712b4
nirohero <nirokal92@gmail.com>
2017-07-16 10:41:49 -0400

    base class complete for now

--- Assignment4_Planets/Planet.cs
+++ Assignment4_Planets/Planet.cs
@@ -8,6 +8,43 @@ namespace Assignment4_Planets
 {
     class Planet
     {
+        /// <summary>
+        /// Private instance variables
+        /// </summary>
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
 
+        /// <summary>
+        /// Public Properties
+        /// </summary>
+        public double Diameter { get; }
+        public double Mass { get; }
+        public int MoonCount { get; }
+        public double OrbitalPeriod { get; set; }
+        public int RingCount { get; set; }
+        public double RotationPeriod { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("name:{0}\n{1}\n{2}",_name,_diameter,_mass);
+        }
     }
 }
