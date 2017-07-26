12fa54fb5a8449ab13df09d8f1e72a99c9b79c72
nirohero <nirokal92@gmail.com>
2017-07-26 01:36:18 -0400

    Added Terrestrial Class that also extends the Planet class

--- Assignment4_Planets/IHabitable.cs
+++ Assignment4_Planets/IHabitable.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-namespace Assignment4_Planets
+/*
+ * Name:Nirojan Kalaimannan
+ * Date: July 26, 2017
+ * Description: This is an interface that determines if a planet is habitable
+ * Version: 0.1 -Created the IHabitable interface
+ */
+namespace Assignment4
 {
     public interface IHabitable
     {
---
+++ Assignment4_Planets/TerrestrialPlanet.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name:Nirojan Kalaimannan
+ * Date: July 26, 2017
+ * Description: This is the TerristrialPlanet class
+ * Version: 0.1 -Created the TerrestrialPlanet class
+ */
+namespace Assignment4
+{
+    public class TerrestrialPlanet : Planet, IHasMoons, IHabitable
+    {
+        //PRIVATE INSTANCE VARIABLES
+        private bool _oxygen;
+        private bool _hasMoons;
+        private bool _habitable;
+
+        public bool HasMoons
+        {
+            get
+            {
+                return this._hasMoons;
+            }
+            set
+            {
+                this._hasMoons = value;
+            }
+        }
+        public bool Habitable
+        {
+            get
+            {
+                return this._habitable;
+            }
+            set
+            {
+                this._habitable = value;
+            }
+        }
+
+        //CONSTRUCTOR
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen)
+        : base(name, diameter, mass)
+        {
+            this._oxygen = oxygen;
+        }
+        public bool Moons()
+        {
+            if (MoonCount < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool isHabitable()
+        {
+            if(_oxygen==false)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
