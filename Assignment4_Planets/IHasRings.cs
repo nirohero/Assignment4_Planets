1a4426c58515e6a6b43acfd25bd7c9a3c1d32a32
nirohero <nirokal92@gmail.com>
2017-07-25 23:52:14 -0400

    Added IHasRings interface

---
+++ Assignment4_Planets/IHasRings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name:Nirojan Kalaimannan
+ * Date: July 25, 2017
+ * Description: This is an interface that defines a ring property
+ * Version: 0.1 -Created the IHasRingsinterace
+ */
+namespace Assignment4_Planets
+{
+    public interface IHasRings
+    {
+        
+        bool hasRings { get; set;}
+    }
+}
