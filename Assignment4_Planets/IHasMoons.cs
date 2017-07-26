c9b22cc95bead4f3bae5ec44e3d64710949a0d34
nirohero <nirokal92@gmail.com>
2017-07-25 22:13:26 -0400

    Added IHasMoons interface

---
+++ Assignment4_Planets/IHasMoons.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name:Nirojan Kalaimannan
+ * Date: July 25, 2017
+ * Description: This is an interface that defines a moon property
+ * Version: 0.1 -Created the IHasMoons interace
+ */
+namespace Assignment4_Planets
+{
+    /// <summary>
+    /// This is the IHasMoons interface
+    /// </summary>
+    public interface IHasMoons
+    {
+        //PUBLIC PROPERTIES
+        bool HasMoons { get; set; }
+    }
+}
